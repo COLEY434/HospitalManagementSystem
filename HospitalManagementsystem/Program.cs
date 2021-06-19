d91f41b9ad2d9ff2e0476de910f1efb5dee2c3d5
collins Anieze <chigoziecollins434@gmail.com>
2021-06-19 21:43:25 +0100

    Configured database migration automatically

--- HospitalManagementsystem/Program.cs
+++ HospitalManagementsystem/Program.cs
@@ -1,5 +1,8 @@
+using HospitalManagementsystem.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,13 +14,38 @@ namespace HospitalManagementsystem
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();//.Run();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+                    logger.LogInformation("Migrating database");
+                    await dbContext.Database.MigrateAsync();
+                    logger.LogInformation("Done Migrating database");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                 .ConfigureLogging(logging =>
+                 {
+                     logging.ClearProviders();
+                     logging.AddConsole();
+                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
